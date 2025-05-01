13da65b800bda3a0b7a7f43a5803dc94dd1665ed
Francesco Adami <francescoadami9@gmail.com>
2025-05-01 15:00:08 +0200

    [TMP] CheckRobot Function

---
+++ Assets/Scripts/RobotCheck.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotCheck : MonoBehaviour
+{
+    public void CheckRobot(Robot robot)
+    {
+
+    }
+}
